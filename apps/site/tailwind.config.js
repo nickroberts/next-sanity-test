e213572aa5a16246d8c1f2bad2194377ab178acf
Nick Roberts <djparadyme@gmail.com>
2021-10-28 15:15:12 -0400

    Update tailwind config

--- apps/site/tailwind.config.js
+++ apps/site/tailwind.config.js
@@ -1,12 +1,11 @@
+const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.tsx', './pages/*.tsx'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  presets: [require('../../tailwind-workspace-preset')],
+  purge: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: false,
 };
---
+++ tailwind-workspace-preset.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
