5130e481fcc5f906239404c45970ad5413c33a63
Nick Roberts <djparadyme@gmail.com>
2021-10-28 14:48:03 -0400

    Add tailwind, stylelint

---
+++ apps/site/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.tsx', './pages/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
